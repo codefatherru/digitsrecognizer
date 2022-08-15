a6a262add6a671277a5733ebb1d1fed014ab43e1
Ildar Bikmamatov <ildar@bayrell.org>
2022-08-16 02:54:35 +0600

    train_epoch_callback

--- mnist_torch2.py
+++ mnist_torch2.py
@@ -8,52 +8,15 @@
 # https://blog.bayrell.org/ru/iskusstvennyj-intellekt/411-obuchenie-mnogoslojnogo-perseptrona-operaczii-xor.html
 ##
 
-import torch, os, sys, math
+import torch, os, math
 import numpy as np
-import matplotlib.pyplot as plt
 
 from torch import nn
-from torchsummary import summary
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import TensorDataset
 
 from ai_helper import *
 
 
-def get_answer_from_vector(vector):
-	r"""
-	Returns answer from vector
-	"""
-	value_max = -math.inf
-	value_index = 0
-	for i in range(0, len(vector)):
-		value = vector[i]
-		if value_max < value:
-			value_index = i
-			value_max = value
-	
-	return value_index
-	
-	
-def get_vector_by_number(count):
-	
-	r"""
-	Преобразует число в списке в выходной вектор
-	Например:
-	1 -> [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-	5 -> [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
-	"""
-	
-	def f(number):
-		res = [0.0] * count
-		
-		if (number >=0 and number < count):
-			res[number] = 1.0
-			
-		return res
-		
-	return f
-	
-	
 def data_normalize_x(data_x):
 	r"""
 	Нормализация датасета по x
@@ -74,7 +37,7 @@ def data_normalize_y(data_y):
 	r"""
 	Нормализация датасета по y
 	"""
-	data_y = list(map(get_vector_by_number(10), data_y))
+	data_y = list(map(get_vector_from_answer(10), data_y))
 	data_y = torch.tensor( data_y )
 	return data_y
 	
@@ -85,11 +48,6 @@ class Network(AbstractNetwork):
 	def __init__(self):
 		
 		AbstractNetwork.__init__(self)
-		
-		self.input_shape = 784
-		self.output_shape = 10
-		self.batch_size = 64
-		self.epochs = 30
 		
 		self.mnist_data = None
 		
@@ -102,6 +60,7 @@ class Network(AbstractNetwork):
 	
 	
 	def load_dataset(self, type):
+		
 		r"""
 		Загрузить датасет
 		"""
@@ -156,8 +115,7 @@ class Network(AbstractNetwork):
 			print ("Model answer", predict)
 			print ("Correct answer", y)
 			
-			plt.imshow(tensor_x, cmap='gray')
-			plt.show()
+			plot_show_image(tensor_x, cmap='gray')
 			
 			if predict == y:
 				res = res + 1
@@ -169,18 +127,29 @@ class Network(AbstractNetwork):
 		
 		AbstractNetwork.create_model(self)
 		
+		self.input_shape = 784
+		self.output_shape = 10
+		self.batch_size = 64
+		self.epochs = 30
+		
 		self.model = nn.Sequential(
 			nn.Linear(self.input_shape, 128),
 			nn.ReLU(),
 			nn.Linear(128, self.output_shape),
 			#nn.Softmax()
-		)
+		)	
+	
+	
+	def train_epoch_callback(self, **kwargs):
+		r"""
+		Train epoch callback
+		"""
 		
-		# Adam optimizer
-		self.optimizer = torch.optim.Adam(self.model.parameters())
+		loss_test = kwargs["loss_test"]
+		step_index = kwargs["step_index"]
 		
-		# mean squared error
-		self.loss = nn.MSELoss()
+		if loss_test < 0.005 and (step_index + 1) >= 5:
+			self.stop_training()
 	
 	
 
