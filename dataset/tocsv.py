c97b1c4bd55dc50877517cb141651eb0ad6bf7f1
codefatherru <vasiliy.m.leonov@gmail.com>
2025-07-15 20:01:28 +0300

    первая версия генератора MNIST CSV

--- dataset/tocsv.py
+++ dataset/tocsv.py
@@ -1,88 +1,66 @@
 # from https://github.com/AhmetTumis/mnist-png-to-csv-converter/blob/main/main.py
 
+from PIL import Image, ImageOps
 import numpy as np
-from PIL import Image
 import os
-import csv
-from pprint import pprint
-import matplotlib.pyplot as plt
-
-def file_indexer():
-    files = []
-    for dirname, dirnames, filenames in os.walk('.'):
-        for subdirname in dirnames:
-            files.append(os.path.join(dirname, subdirname))
-
-        for filename in filenames:
-            files.append(os.path.join(dirname, filename))
-    print('file_indexer:')
-    pprint(files)
-
-    writeToFile = []
-    writeToTestFile = []
-
-    for file in files:
-        if ".png" in file and "train" in file:
-            writeToFile.append(file)
-        if ".png" in file and "test" in file:
-            writeToTestFile.append(file)
-
-    with open("Files.csv", "w") as f:
-        write = csv.writer(f)
-        write.writerows(writeToFile)
-        print('writeToFile')
-        pprint(writeToFile)
-
-    with open("TestFiles.csv", "w") as f:
-        write = csv.writer(f)
-        write.writerows(writeToTestFile)
-        print('writeToFile')
-        pprint(writeToFile)
-        print('writeToTestFile')
-        pprint(writeToTestFile)
 
 
-def write_to_csv(trainOrTest, label, contents):
-    arrayToDump = np.insert(contents, 0, label)
-    pprint(arrayToDump)
-    arrayToDump = arrayToDump.reshape((1, 785))
 
-    dataFileName = ""
-    if trainOrTest:
-        dataFileName = "train.csv"
-    else:
-        dataFileName = "test.csv"
+def roman_to_arabian(letter):
+    if letter == 'x':
+        return 10
+    elif letter == 'v':
+        return 5
+    return None
 
-    with open(dataFileName, "a") as f:
-        np.savetxt(f, arrayToDump, fmt="%d", delimiter=",")
+def png_to_csv(png):
+    image_file = Image.open(png)
+    inverted_img = ImageOps.invert(image_file)
+    img_grey = inverted_img.convert('L') # convert image to grayscale
+    #image_file.save('testimage.gif')
 
-
-def open_image(pathToFileIndex, trainOrTest):
-    print('open_image читаем '+pathToFileIndex)
-
-    with open(pathToFileIndex, "r") as f:
-        fileList = csv.reader(f)
-        print(fileList)
-
-        for file in fileList:
-            fileName = "".join(file)
-            print(fileName)
-            print(fileName)
-            label = fileName[(fileName.rfind("/") - 1)]
-            print(label)
-            image = Image.open(fileName)
-
-            numpydata = np.asarray(image)
-            print(numpydata)
-            print(numpydata[0])
-            # Загрузим из датасета определенное фото и отобразим его на экране. Должна отобразиться цифра, которая находится на позиции photo_number.
-            print("Number:" )
-            plt.imshow(numpydata, cmap='gray')
-            plt.show()  #формат данных такой же, как и в my.py
-            write_to_csv(trainOrTest, label, numpydata)
-            exit(-1)
+    value = np.asarray(img_grey.getdata(), dtype=np.integer ).reshape((img_grey.size[1], img_grey.size[0]))
+    #print(value)
+    # перевели матрицу в массив
+    fvalue = value.flatten()
+    #print(fvalue)
+    return fvalue
 
 
-file_indexer()
-open_image("Files.csv", True)
-open_image("TestFiles.csv", False)
+
+files = {}
+
+for dirname, dirnames, filenames in os.walk('.'):
+
+
+    for filename in filenames:
+        #print(dirname)
+        print(filename)
+        dirs = dirname.split(os.path.sep)
+        #print(dirs)
+
+        if len(dirs) > 2 and '.png' in filename:
+            letter = dirs[-1]
+            vol = dirs[-2]
+            #print(vol, letter)
+
+            line = png_to_csv(os.path.join(dirname, filename))
+
+            # вставим в первую позицию значение картинки
+            arrayToDump = np.insert(line, 0, roman_to_arabian(letter))
+            #print(arrayToDump)
+            if not (vol in files):
+                #print(vol)
+                files[vol] = []
+            print(len(files[vol]))
+            files[vol].append(arrayToDump)
+
+
+for k, v in files.items():
+    print(k)
+    #print(v)
+    with open('mnist_'+ k +'.csv', "a") as f:
+        np.savetxt(f, v, fmt="%d", delimiter=",")
+
+
+
