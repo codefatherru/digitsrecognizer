c694767807b2394b3ba81482dcc93db816d10042
codefatherru <vasiliy.m.leonov@gmail.com>
2025-07-16 15:51:59 +0300

    шаг2 применяем пороговую обработку

--- img.py
+++ img.py
@@ -27,6 +27,9 @@ def rec_digit(img_path):
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
     gray = 255 - img
 
+    # применяем пороговую обработку
+    (thresh, gray) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
     gray = cv2.resize(gray, (28, 28))
     cv2.imwrite('gray.png', gray)
     #img = gray / 255.0
